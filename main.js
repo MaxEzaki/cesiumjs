d0c70a0adfb5a547c01fa85a36fb4f4297e2cfaf
maxKT <max.perc+sys@gmail.com>
2019-02-01 16:49:09 +0900

    富士山にPOIうち、ドラッグ&ドロップ

--- main.js
+++ main.js
@@ -10,8 +10,8 @@
 
   /*** */
   var viewer = new Cesium.Viewer('cesium', {
-    infoBox: false, //Disable InfoBox widget
-    selectionIndicator: false, //Disable selection indicator
+    // infoBox: false, //Disable InfoBox widget
+    // selectionIndicator: false, //Disable selection indicator
     shouldAnimate: true, // Enable animations
     terrainProvider: Cesium.createWorldTerrain()
 });
@@ -101,69 +101,294 @@ var entity = viewer.entities.add({
     }
 });
 
-//Add button to view the path from the top down
-Sandcastle.addDefaultToolbarButton('View Top Down', function() {
-    viewer.trackedEntity = undefined;
-    viewer.zoomTo(viewer.entities, new Cesium.HeadingPitchRange(0, Cesium.Math.toRadians(-90)));
-});
+// イベントハンドラー 定義
+var handler,
+    longitude,
+    latitude,
+    pinBuilder = new Cesium.PinBuilder(),
+    // promise,
+    height,
+    handler = new Cesium.ScreenSpaceEventHandler(viewer.canvas),
+    pin_position,
+    scene = viewer.scene,
+    dragging = false
+    ;
 
-//Add button to view the path from the side
-Sandcastle.addToolbarButton('View Side', function() {
-    viewer.trackedEntity = undefined;
-    viewer.zoomTo(viewer.entities, new Cesium.HeadingPitchRange(Cesium.Math.toRadians(-90), Cesium.Math.toRadians(-15), 7500));
-});
+/**
+ * 左ダブルクリック時アクション
+ * アラートで経度緯度と標高表示
+ */
+handler.setInputAction(
+    function(movement) {
 
-//Add button to track the entity as it moves
-Sandcastle.addToolbarButton('View Aircraft', function() {
-    viewer.trackedEntity = entity;
-});
+        // POIの設置する
+        if( longitude!=null && latitude!=null && height!=null){
+            console.log('通るよ001');
 
-//Add a combo box for selecting each interpolation mode.
-Sandcastle.addToolbarMenu([{
-    text : 'Interpolation: Linear Approximation',
-    onselect : function() {
-        entity.position.setInterpolationOptions({
-            interpolationDegree : 1,
-            interpolationAlgorithm : Cesium.LinearApproximation
-        });
-    }
-}, {
-    text : 'Interpolation: Lagrange Polynomial Approximation',
-    onselect : function() {
-        entity.position.setInterpolationOptions({
-            interpolationDegree : 5,
-            interpolationAlgorithm : Cesium.LagrangePolynomialApproximation
-        });
-    }
-}, {
-    text : 'Interpolation: Hermite Polynomial Approximation',
-    onselect : function() {
-        entity.position.setInterpolationOptions({
-            interpolationDegree : 2,
-            interpolationAlgorithm : Cesium.HermitePolynomialApproximation
-        });
-    }
-}], 'interpolationMenu');
+            console.log( `経度：${longitude} \n緯度：${latitude} \n標高：${height}` );
+            putPOI(longitude,latitude,height);
 
-
-handler = new Cesium.ScreenSpaceEventHandler(scene.canvas);
-    handler.setInputAction(function(movement) {
-        var cartesian = viewer.camera.pickEllipsoid(movement.endPosition, scene.globe.ellipsoid);
-        if (cartesian) {
-            var cartographic = Cesium.Cartographic.fromCartesian(cartesian);
-            var longitudeString = Cesium.Math.toDegrees(cartographic.longitude).toFixed(2);
-            var latitudeString = Cesium.Math.toDegrees(cartographic.latitude).toFixed(2);
-
-            entity.position = cartesian;
-            entity.label.show = true;
-            entity.label.text =
-                'Lon: ' + ('   ' + longitudeString).slice(-7) + '\u00B0' +
-                '\nLat: ' + ('   ' + latitudeString).slice(-7) + '\u00B0';
-        } else {
-            entity.label.show = false;
+            // sampleHeights();
+            // console.log(promise);
         }
-    }, Cesium.ScreenSpaceEventType.MOUSE_MOVE);
+        else{
+            alert('ないよ');
+        }
 
-viewer.zoomTo(viewer);
+    }, Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK
+);
+
+/**
+ * 左クリックダウン時
+ * POIの位置を変える
+ */
+handler.setInputAction(
+    function(click) {
+        console.log(`左クリックしたよ=====`);
+        var pickedObject = scene.pick(click.position);
+        // console.log(pickedObject);
+        // console.log(Cesium.defined(pickedObject));
+        // console.log(pickedObject.id);
+        console.log(yellowPin.billboard);
+        // yellowPin = 富士山登頂に置いてるPIN。このインスタンスは動的に生成させたほうがよろしいね。
+        if (Cesium.defined(pickedObject) && (pickedObject.id === yellowPin)) {
+console.log(`動かすよ！`);
+            yellowPin.billboard.scale = 1.2;
+            dragging = true;
+            scene.screenSpaceCameraController.enableRotate = false;
+        }
+console.log(`LEFT_DOWN>>>ドラッグ状況：${dragging}`);
+    },
+    Cesium.ScreenSpaceEventType.LEFT_DOWN
+);
+
+/**
+ * 左クリックアップ時
+ */
+handler.setInputAction(
+    function() {
+        yellowPin.billboard.scale = 1;
+        dragging = false;
+        scene.screenSpaceCameraController.enableRotate = true;
+console.log(`LEFT_UP>>>ドラッグ状況：${dragging}`);
+    },
+    Cesium.ScreenSpaceEventType.LEFT_UP
+);
+
+/**
+ * マウスオーバー時＞＞経度緯度取得
+ * クリックだと取得できない
+ * 高さの取得についての参照先[https://groups.google.com/forum/#!topic/cesium-dev/kDIs_j9zKNI]
+ */
+handler.setInputAction(
+    function(movement) {
+        var cartesian = viewer.camera.pickEllipsoid(movement.endPosition, viewer.scene.globe.ellipsoid);
+        //マウスが地球上にあることを
+        if (cartesian) {
+            //位置情報を管理するオブジェクトcartographicを取得
+            var cartographic = Cesium.Cartographic.fromCartesian(cartesian);
+            //緯度経度を小数点10桁で取得
+            // longitude = Cesium.Math.toDegrees(cartographic.longitude).toFixed(5);
+            // latitude = Cesium.Math.toDegrees(cartographic.latitude).toFixed(5);
+
+            longitude = Cesium.Math.toDegrees(cartographic.longitude).toFixed(10);
+            latitude = Cesium.Math.toDegrees(cartographic.latitude).toFixed(10);
+
+            var ray = viewer.camera.getPickRay(movement.endPosition);
+            var position = viewer.scene.globe.pick(ray, viewer.scene);
+            if (Cesium.defined(position)) {
+                var cartographic = Cesium.Ellipsoid.WGS84.cartesianToCartographic(position);
+                height = cartographic.height
+            }
+            // console.log('リアルタイム表示＝＝＝＝＝＝＝＝＝＝');
+            // console.log( `経度：${longitude} \n緯度：${latitude} \n標高：${height}` );
+
+            // マウス横の標識を設定＞２Dでしか表示されないのか、これ
+            markEntity.position = cartesian;
+            markEntity.label.show = true;
+            markEntity.label.text =
+                'Lon: ' + ('   ' + longitude).slice(-7) + '\u00B0' +
+                '\nLat: ' + ('   ' + latitude).slice(-7) + '\u00B0';
+
+            // ドラッグ処理
+            if (dragging) {
+console.log(`ドラッグしてるよ!!`);
+                yellowPin.position = viewer.camera.pickEllipsoid(movement.endPosition);
+            }
+
+        } else {
+            console.log('地球外！！');
+            markEntity.label.show = false;
+        }
+    }, Cesium.ScreenSpaceEventType.MOUSE_MOVE
+);
+
+
+/**
+ * 青点を設置＞テスト
+ */
+var redPin = viewer.entities.add({
+    name : 'Blank red pin',
+    // 指定の場所にポイントを設置
+    // position : Cesium.Cartesian3.fromDegrees(-112.1213174224, 36.0902161932, 1111.3939500467302),
+    position : Cesium.Cartesian3.fromDegrees(138.433762,35.215631,1111.3939500467302),
+    billboard : {
+        show : false,
+        image : pinBuilder.fromColor(Cesium.Color.ROYALBLUE, 48).toDataURL(),
+        horizontalOrigin:Cesium.HorizontalOrigin.BOTTOM,
+        verticalOrigin : Cesium.VerticalOrigin.BOTTOM
+    },
+    point : {
+        pixelSize : 20,
+        color : Cesium.Color.RED,
+        outlineWidth: 2,
+        outlineColor: Cesium.Color.WHITE
+    }
+});
+
+var greenPin = viewer.entities.add({
+    name : 'Blank green pin',
+    // 指定の場所にポイントを設置
+    // position : Cesium.Cartesian3.fromDegrees(-112.1213174224, 36.0902161932, 1111.3939500467302),
+    position : Cesium.Cartesian3.fromDegrees(138.433762, 35.215631, 1111.3939500467302),
+    billboard : {
+        show : false,
+        image : pinBuilder.fromColor(Cesium.Color.ROYALBLUE, 48).toDataURL(),
+        horizontalOrigin:Cesium.HorizontalOrigin.CENTER,
+        verticalOrigin : Cesium.VerticalOrigin.CENTER
+    },
+    point : {
+        pixelSize : 10,
+        color : Cesium.Color.GREEN,
+        outlineWidth: 2,
+        outlineColor: Cesium.Color.WHITE
+    }
+});
+
+/**
+ * 富士山の登頂
+ */
+var yellowPin = viewer.entities.add({
+    name : 'Blank yellow pin',
+    // position : Cesium.Cartesian3(-3915852.0, 3436878.0, 3673245.0),
+    position : Cesium.Cartesian3.fromDegrees(138.72711666666666, 35.36564166666666, 3800.000),
+    
+    billboard : {
+        scale: 1
+    },
+
+    point : {
+        pixelSize :20,
+        // color : Cesium.Color.TRANSPARENT,
+        color : Cesium.Color.YELLOW,
+        outlineColor : Cesium.Color.YELLOW,
+        outlineWidth : 3
+    }
+});
+
+/**
+ * 標識表示
+ */
+var markEntity = viewer.entities.add({
+    label : {
+        show : false,
+        showBackground : true,
+        font : '14px monospace',
+        horizontalOrigin : Cesium.HorizontalOrigin.LEFT,
+        verticalOrigin : Cesium.VerticalOrigin.TOP,
+        pixelOffset : new Cesium.Cartesian2(15, 0)
+    }
+});
+
+/**
+ * ピンのposition変更
+ */
+function putPOI(arg_longitude, arg_latitude, arg_height){
+
+    if( arg_longitude==null&&arg_latitude==null&&arg_height==null ){
+        console.log('ピン止められないよ！');
+        return;
+    }
+console.log(`ここにピンおくよ！！！`);
+console.log( `経度：${arg_longitude} \n緯度：${arg_latitude} \n標高：${arg_height}` );
+    viewer.entities.add({
+        name : 'POI pin',
+
+        position : Cesium.Cartesian3.fromDegrees(arg_longitude, arg_latitude, arg_height),
+        point : {
+            pixelSize : 8,
+            color : Cesium.Color.TRANSPARENT,
+            outlineColor : Cesium.Color.YELLOW,
+            outlineWidth : 3
+        }
+/***
+        // 指定の場所にポイントを設置
+        // position : Cesium.Cartesian3.fromDegrees(-112.13315, 36.14703, 2200),
+        position: Cesium.Cartesian3.fromDegrees(arg_longitude, arg_latitude, arg_height),
+        billboard : {
+            show : false,
+            image : pinBuilder.fromColor(Cesium.Color.ROYALBLUE, 48).toDataURL(),
+            verticalOrigin : Cesium.VerticalOrigin.BOTTOM
+        },
+        point : {
+            pixelSize : 20,
+            color : Cesium.Color.ROYALBLUE,
+            outlineWidth: 2,
+            outlineColor: Cesium.Color.WHITE
+        }
+ */
+
+    });
+
+}
+
+/**
+ * 3Dでのポイント設置
+ * 参照：[https://cesiumjs.org/Cesium/Apps/Sandcastle/?src=Sample%20Height%20from%203D%20Tiles.html&label=3D%20Tiles]
+ */
+function sampleHeights(){
+console.log('sampleHeights========');
+    if (!scene.clampToHeightSupported) {
+        console.log('This browser does not support clampToHeightMostDetailed.');
+    }
+    console.log('sampleHeights 010');
+    // viewer.entities.removeAll();
+
+    var cartesian1 = new Cesium.Cartesian3(1216390.063324395, -4736314.814479433, 4081341.9787972216);
+    var cartesian2 = new Cesium.Cartesian3(1216329.5413318684, -4736272.029009798, 4081407.9342479417);
+
+    var count = 30;
+    var cartesians = new Array(count);
+    for (var i = 0; i < count; ++i) {
+        var offset = i / (count - 1);
+        cartesians[i] = Cesium.Cartesian3.lerp(cartesian1, cartesian2, offset, new Cesium.Cartesian3());
+    }
+console.log(cartesians);
+    scene.clampToHeightMostDetailed(cartesians).then(function(clampedCartesians) {
+// console.log(clampedCartesians);
+console.log('sampleHeights 020');
+        for (var i = 0; i < count; ++i) {
+console.log(clampedCartesians[i]);
+            viewer.entities.add({
+                // position: clampedCartesians[i],
+                position: Cesium.Cartesian3.fromDegrees(-112.0203879773 , 36.0810284104 , 788.782136395306),
+                ellipsoid : {
+                    radii : new Cesium.Cartesian3(2, 2, 2),
+                    material : Cesium.Color.RED
+                }
+            });
+        }
+    });
+}
+
+
+
+//Since some of the pins are created asynchronously, wait for them all to load before zooming/
+// Cesium.when.all([bluePin], function(pins){
+//     viewer.zoomTo(pins);
+// });
+
+viewer.zoomTo(yellowPin);
+
 }());
 
